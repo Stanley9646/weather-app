ed3efcd9776fc3cc3f674edc8bb7c507e0ca9bb1
Stanley Githinji <stanogg1@gmail.com>
2023-03-06 01:09:05 -0800

    feat: weather app

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import "./App.css";
+import Search from "./components/search/search";
+import Forecast from "./components/forecast/forecast";
+import CurrentWeather from "./components/current-Weather/current-weather";
+import { WEATHER_API_URL, WEATHER_API_KEY } from "./api";
+import { useState } from "react";
+function App() {
+  const [currentWeather, setCurrentWeather] = useState();
+  const [forecastWeather, setForecastWeather] = useState();
+  const handleOnSearchChange = (searchData) => {
+    const [lat, lon] = searchData.value.split("");
+    const currentWeatherFetch = fetch(
+      `${WEATHER_API_URL}/weather?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=metric`
+    );
+    const forecastFetch = fetch(
+      `${WEATHER_API_URL}/forecast?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=metric`
+    );
+    Promise.all([currentWeatherFetch, forecastFetch])
+      .then(async (response) => {
+        const weatherResponse = await response[0].json();
+        const forecastResponse = await response[1].json();
+
+        setCurrentWeather({ city: searchData.label, ...weatherResponse });
+        setForecastWeather({ city: searchData.label, ...forecastResponse });
+      })
+      .catch((err) => console.log(err));
+  };
+  console.log({ currentWeather });
+  console.log({ forecastWeather });
+  return (
+    <div className="Container">
+      <Search onSearchChange={handleOnSearchChange} />
+      {CurrentWeather && <currentWeather data={currentWeather} />}
+      {forecastWeather && <Forecast data={forecastWeather} />}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/api.js
@@ -0,0 +1,11 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '81d00fb74dmsh45a0970f819cc2fp1c50a0jsn460e47f73168',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL ="https://wft-geo-db.p.rapidapi.com/v1/geo"
+export const WEATHER_API_URL ="https://api.openweathermap.org/data/2.5"
+export const WEATHER_API_KEY="e2d70cdc692fc5a2e7f3c06b07f3a563"
